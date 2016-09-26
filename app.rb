9291384e2c32a94f23e3c417ad985ece01ddf4e2
lilybarrett <lily@barrett.us>
2016-09-26 16:07:39 -0400

    complete ajax demo app

--- app.rb
+++ app.rb
@@ -2,6 +2,7 @@ require 'sinatra'
 require 'sinatra/activerecord'
 require 'sinatra/reloader'
 require 'sinatra/flash'
+require "json"
 
 enable :sessions
 
@@ -21,6 +22,33 @@ Dir[File.join(File.dirname(__FILE__), 'app', '**', '*.rb')].each do |file|
 end
 
 get '/' do
-  @title = "Hello World"
-  erb :index
+  redirect "/items"
+end
+
+get "/items" do
+  @items = Item.all
+  erb :items
+end
+
+post "/items" do
+  if params[:name]
+    @item = Item.create({ name: params[:name] })
+  end
+  redirect "/items"
+end
+
+get "/api/v1/items.json" do
+  items = Item.all
+  items.map(&:to_json).to_json
+end
+
+post "/api/v1/items.json" do
+  if !(params[:name].strip.empty?)
+    item = Item.create({ name: params[:name] })
+
+    status 201
+    headers "Location" => "/items/#{item.id}"
+  else
+    status 400
+  end
 end
--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,1 +1,18 @@
+$(document).ready(function() {
+  $('#item-form').on("submit", function(event) {
+    event.preventDefault();
 
+    var newItem = $('#item_content').val();
+
+    var request = $.ajax({
+      method: "POST",
+      url: "/api/v1/items.json",
+      data: { name: newItem }
+    }).success(function(data) {
+      $("ul").append("<li>" + newItem + "</li>");
+      alert("New bucket list item added!");
+    }).error(function(data) {
+      alert("Invalid bucket list submission! Try again!")
+    })
+  });
+})
